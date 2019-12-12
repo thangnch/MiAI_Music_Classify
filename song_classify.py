1d0cfc40026ed81e199e2b2b58b8a6f7ad75a860
thangnch <thangnch@gmail.com>
2019-12-12 12:57:25 +0700

    Add files via upload

--- song_classify.py
+++ song_classify.py
@@ -3,56 +3,62 @@ import librosa.feature
 import librosa.display
 import glob
 import numpy as np
-#import matplotlib.pyplot as plt
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Dropout
 from keras.utils.np_utils import to_categorical
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 
-def extract_features_song(f):
+# Ham trich xuat MFCC cua bai hat
+def get_mfcc(f):
     y, _ = librosa.load(f)
 
-    # get Mel-frequency cepstral coefficients
-    mfcc = librosa.feature.mfcc(y)
-    # normalize values between -1,1 (divide by max)
-    mfcc /= np.amax(np.absolute(mfcc))
+    # Trich xua MFCC
+    my_features = librosa.feature.mfcc(y)
+    # Normalize gia tri MFCC
+    my_features /= np.amax(np.absolute(my_features))
 
-    return np.ndarray.flatten(mfcc)[:25000]
+    # Flattern MFCC
+    return np.ndarray.flatten(my_features)[:25000]
 
-def generate_features_and_labels():
-    all_features = []
-    all_labels = []
+# Load du lieu
+def load_data():
+    mfcc_list = []
+    label_list = []
+    # Dinh nghia cac class
+    song_type = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
 
-    genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
-    for genre in genres:
-        sound_files = glob.glob('data/genres/'+genre+'/*.wav')
-        print('Processing %d songs in %s genre...' % (len(sound_files), genre))
+    # Load cac file tu folder
+    for curr_type in song_type:
+        sound_files = glob.glob('data/'+curr_type+'/*.wav')
         for f in sound_files:
-            features = extract_features_song(f)
-            all_features.append(features)
-            all_labels.append(genre)
+            # Trich xuat MFCC
+            features = get_mfcc(f)
+            mfcc_list.append(features)
+            # Them label
+            label_list.append(curr_type)
 
     # convert labels to one-hot encoding
-    label_uniq_ids, label_row_ids = np.unique(all_labels, return_inverse=True)
+    label_uniq_ids, label_row_ids = np.unique(label_list, return_inverse=True)
     label_row_ids = label_row_ids.astype(np.int32, copy=False)
     onehot_labels = to_categorical(label_row_ids, len(label_uniq_ids))
-    return np.stack(all_features), onehot_labels
+    return np.stack(mfcc_list), onehot_labels
 
-features, labels = generate_features_and_labels()
+# Load du lieu
+features, labels = load_data()
 
-print(np.shape(features))
-print(np.shape(labels))
-
+# Tham so chia train/test
 training_split = 0.8
 
-# last column has genre, turn it into unique ids
+# Noi labels vao feature
 alldata = np.column_stack((features, labels))
 
+# Random xao tron data
 np.random.shuffle(alldata)
+
+# Thuc hien chia train/test
 splitidx = int(len(alldata) * training_split)
 train, test = alldata[:splitidx,:], alldata[splitidx:,:]
 
-print(np.shape(train))
-print(np.shape(test))
 
 train_input = train[:,:-10]
 train_labels = train[:,-10:]
@@ -60,21 +66,30 @@ train_labels = train[:,-10:]
 test_input = test[:,:-10]
 test_labels = test[:,-10:]
 
-print(np.shape(train_input))
-print(np.shape(train_labels))
 
-
+#Dinh nghia model
 model = Sequential([
-    Dense(500, input_dim=np.shape(train_input)[1]),
+    Dense(1024, input_dim=np.shape(train_input)[1]),
+    Dense(512, input_dim=1024),
+    Dropout(0.5),
     Activation('relu'),
     Dense(10),
     Activation('softmax'),
     ])
 
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-model.fit(train_input, train_labels,validation_data=(test_input,test_labels), epochs=100, batch_size=32,
-          validation_split=0.2)
+file_path = "best_model_{epoch:08d}_{val_loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath=file_path, monitor='val_accuracy', verbose=1, save_best_only=True, mode='auto')
+callbacks_list = [checkpoint]
 
+# Tien hanh train
+model.fit(train_input, train_labels, epochs=100, batch_size=32,
+          validation_split=0.2, callbacks = callbacks_list)
+
+# Tien hanh eval tren tap test
 loss, acc = model.evaluate(test_input, test_labels, batch_size=32)
 
 print("Done!")
